a90e8b4af81fe1f4102df95efc1b027254b5d33b
aiden <aiden@DESKTOP-DGFUO2Q>
2016-12-19 14:51:15 +0900

    practice

--- src/practice/model/PracticeService.java
+++ src/practice/model/PracticeService.java
@@ -1,5 +1,11 @@
 package practice.model;
 
+import java.io.*;
+import java.util.*;
+
+import javax.imageio.stream.*;
+import javax.servlet.*;
+
 import org.apache.ibatis.session.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
@@ -8,25 +14,43 @@ import org.springframework.stereotype.*;
 public class PracticeService {
 	@Autowired
 	SqlSessionFactory fac;
+	@Autowired
+	ServletContext application;
 	
 	public String content(String content){
-		String s = content.substring(content.indexOf('{')+1, content.lastIndexOf('}'));
-		if(s.contains("public void main(String[] args)")){
-			s = s.substring(s.indexOf('{')+1, s.lastIndexOf('}'));
-		} else {
-			return "";
-		}
-		if(s.contains("System")){
-			if(s.contains("out")){
-				if(s.contains("println")){
-					if(s.contains("\"")){
-						return s.substring(s.indexOf('"')+1, s.lastIndexOf('"'));
-					} else {
-						return s.substring(s.indexOf('(')+1, s.lastIndexOf(')'));
-					}
-				}
+		String uuid = UUID.randomUUID().toString().substring(0, 10);
+		String dir = application.getRealPath("/files");
+		File f = new File(dir, uuid+".java");
+		try{
+			FileOutputStream fos = new FileOutputStream(f);
+			byte[] ar = content.getBytes();
+			for(int i=0; i<ar.length; i++){
+				fos.write(ar[i]);
 			}
+//			FileInputStream fis = new FileInputStream(f);
+			System.out.println("파일생성완료");
+			System.out.println(uuid);
+		} catch(Exception e){
+			e.printStackTrace();
+			return null;
 		}
+//		String s = content.substring(content.indexOf('{')+1, content.lastIndexOf('}'));
+//		if(s.contains("public void main(String[] args)")){
+//			s = s.substring(s.indexOf('{')+1, s.lastIndexOf('}'));
+//		} else {
+//			return "";
+//		}
+//		if(s.contains("System")){
+//			if(s.contains("out")){
+//				if(s.contains("println")){
+//					if(s.contains("\"")){
+//						return s.substring(s.indexOf('"')+1, s.lastIndexOf('"'));
+//					} else {
+//						return s.substring(s.indexOf('(')+1, s.lastIndexOf(')'));
+//					}
+//				}
+//			}
+//		}
 		return content;
 	}
 }
