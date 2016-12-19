81f1a0661db426d5c9f3c8678179bbb899e1cc06
aiden <aiden@DESKTOP-DGFUO2Q>
2016-12-19 16:18:23 +0900

    practice / pom.xml

--- src/practice/model/PracticeService.java
+++ src/practice/model/PracticeService.java
@@ -6,6 +6,7 @@ import java.util.*;
 import javax.imageio.stream.*;
 import javax.servlet.*;
 
+import org.apache.commons.exec.*;
 import org.apache.ibatis.session.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
@@ -17,10 +18,11 @@ public class PracticeService {
 	@Autowired
 	ServletContext application;
 	
-	public String content(String content){
-		String uuid = UUID.randomUUID().toString().substring(0, 10);
+	/*public String content(String content, String title){
+//		String uuid = UUID.randomUUID().toString().substring(0, 10);
 		String dir = application.getRealPath("/files");
-		File f = new File(dir, uuid+".java");
+		String s;
+		File f = new File(dir, title+".java");
 		try{
 			FileOutputStream fos = new FileOutputStream(f);
 			byte[] ar = content.getBytes();
@@ -29,28 +31,74 @@ public class PracticeService {
 			}
 //			FileInputStream fis = new FileInputStream(f);
 			System.out.println("파일생성완료");
-			System.out.println(uuid);
+			System.out.println(title);
+			System.out.println(dir);
+			
+			Process proc = new ProcessBuilder("cmd", "cd ", dir).start();
+			System.out.println("cmd 경로이동");
+			proc = new ProcessBuilder("cmd", "javac ", title, ".java").start();
+			System.out.println("cmd 컴파일완료");
+			proc = new ProcessBuilder("cmd", "java ", title).start();
+			System.out.println("cmd 실행완료");
+			BufferedReader br   = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+			System.out.println(br);
+			while ((s =   br.readLine()) != null) System.out.println(s);
+			System.out.println("======");
+//			System.out.println(proc.exitValue());
 		} catch(Exception e){
 			e.printStackTrace();
 			return null;
 		}
-//		String s = content.substring(content.indexOf('{')+1, content.lastIndexOf('}'));
-//		if(s.contains("public void main(String[] args)")){
-//			s = s.substring(s.indexOf('{')+1, s.lastIndexOf('}'));
-//		} else {
-//			return "";
-//		}
-//		if(s.contains("System")){
-//			if(s.contains("out")){
-//				if(s.contains("println")){
-//					if(s.contains("\"")){
-//						return s.substring(s.indexOf('"')+1, s.lastIndexOf('"'));
-//					} else {
-//						return s.substring(s.indexOf('(')+1, s.lastIndexOf(')'));
-//					}
-//				}
-//			}
-//		}
+		return content;
+	}*/
+	public String content(String content, String title){
+		String dir = application.getRealPath("/files");
+		File f = new File(dir, title+".java");
+		try{
+			FileOutputStream fos = new FileOutputStream(f);
+			byte[] ar = content.getBytes();
+			for(int i=0; i<ar.length; i++){
+				fos.write(ar[i]);
+			}
+//			FileInputStream fis = new FileInputStream(f);
+			System.out.println("파일생성완료");
+			System.out.println(title);
+			System.out.println(dir);
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		DefaultExecutor executor = new DefaultExecutor();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(baos);
+        executor.setStreamHandler(streamHandler);
+        CommandLine commandLine = CommandLine.parse("javac");
+
+        // 파라미터가 필요할경우
+
+        commandLine.addArguments(dir+"\\"+title+".java");
+
+        try {
+        	System.out.println(commandLine);
+            int exitCode = executor.execute(commandLine);
+            System.out.println(new String(baos.toByteArray()));
+            System.out.println("exit " + exitCode);
+            
+            commandLine = CommandLine.parse("cd");
+            commandLine.addArgument(dir);
+            System.out.println(commandLine);
+            exitCode = executor.execute(commandLine);
+            System.out.println("exit2 "+exitCode);
+            
+            commandLine = CommandLine.parse("java");
+            commandLine.addArgument(title);
+            System.out.println(commandLine);
+            exitCode = executor.execute(commandLine);
+            System.out.println("exit3 "+exitCode);
+        } catch (ExecuteException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 		return content;
 	}
 }
